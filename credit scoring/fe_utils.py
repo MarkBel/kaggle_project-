b9fdd8f4272572d04ec77b86fcbe97371be7d517
Dmitry Rusak <dmitry.rusak@navigaglobal.com>
2021-07-02 20:41:21 +0300

    add utils for feature extract

---
+++ credit scoring/fe_utils.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pandas as pd 
+import warnings
+from typing import List
+warnings.filterwarnings(action="ignore")
+
+def extract_payd_period(x: List) -> List:
+    lst = []
+    for i in range(1, len(x)):
+        delta = (x[i] - x[i - 1]).days
+        lst.append(delta)
+    return lst
+
+def extract_delay_day(x: List) -> int:
+    count = 0
+    for i in x:
+        if i > 31:
+            count += i - 30
+    return count
+
+def extract_amount_delays(x: List) -> int:
+    count = 0
+    for i in x:
+        if i > 31:
+            count += 1
+    return count
+
+def extract_amount_early(x: List) -> int:
+    count = 0
+    for i in x:
+        if i < 28:
+            count += 1
+    return count
+
+def extract_amount_intime(x: List) -> int:
+    count = 0
+    for i in x:
+        if i > 27 and i < 32:
+            count += 1
+    return count
+
+def interpl_targ(df: pd.DataFrame) -> None:
+    window = 2
+    df["ExpectedTermDate"] = pd.to_datetime(df["ExpectedTermDate"]) + pd.to_timedelta(pd.np.ceil(df.Term*window), unit="D")
+    df["LastPaymentDate"] = pd.to_datetime(df["LastPaymentDate"]).dt.tz_localize(None)
+    culc_fild = []
+    for r in df.iterrows():
+        r = r[1]
+        culc = ((100 * sum((r.PaymentsHistory) + r[-6:].values.tolist()) // r.TotalContractValue)>=60.) and (r.LastPaymentDate < r.ExpectedTermDate)
+        culc_fild.append(float(culc))
+
+    df["culc_fild"] = culc_fild
+    
+
+def pad_history(df:pd.DataFrame, max_len:int=41) -> None:
+    
+    padded_payments = []
+    
+    for r in df.copy().iterrows():
+        r = r[1]
+        
+        if len(r.PaymentsHistory) > max_len:
+            padded_payments.append(r.PaymentsHistory[:max_len])
+            
+        else:
+            padding_len = abs(max_len - len((r.PaymentsHistory)))
+            padded_payments.append(r.PaymentsHistory + padding_len*[0.])
+            
+    
+    df["PaymentsHistory"] = padded_payments
+
+
+def create_extra_features(df: pd.DataFrame) -> None:
+    df['NANs_cnt'] = df.isnull().sum(axis = 1)
+    
+def create_col_with_min_freq(data, col, min_freq = 10):
+    data[col + '_fixed'] = data[col].astype(str)
+    data.loc[data[col + '_fixed'].value_counts()[data[col + '_fixed']].values < min_freq, col + '_fixed'] = "RARE_VALUE"
+    data.replace({'nan': np.nan}, inplace = True)
+
+def create_gr_feats(data,cat_cols,num_cols):
+    for cat_col in cat_cols:
+        create_col_with_min_freq(data, cat_col, 25)
+        for num_col in num_col:
+            for n, f in [('mean', np.mean), ('min', np.nanmin), ('max', np.nanmax)]:
+                data['FIXED_' + n + '_' + num_col + '_by_' + cat_col] = data.groupby(cat_col + '_fixed')[num_col].transform(f)
+    for col in cat_cols+num_cols:
+        data[col + '_cnt'] = data[col].map(data[col].value_counts(dropna = False))
