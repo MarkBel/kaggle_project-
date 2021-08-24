ea4939a3b12883e9c2abdb5e69e65895ce460eee
Rusak Dima <dmitry.rusak@navigaglobal.com>
2021-08-24 19:48:39 +0300

    create structure of voter

--- credit scoring/src/models/Voter.py
+++ credit scoring/src/models/Voter.py
@@ -1,17 +1,19 @@
+from typing import List, Any
 
 
-class VoterClassifier():
+class Voter():
 
-    
-    def __init__(self, estimators, mode="hard", weight=None, show_info="percent"):
+    def __init__(self, estimators: List[str, Any], mode: str = "hard", task: str = "classification"):
         self.estimators = estimators
         self.mode = mode
-        self.weight = weight
-        self.show_info = show_info
+        self.task = task
 
-    
-    def predict(self,test):
-        if self.mode == "hard":
-            pass
-        elif self.mode == "soft":
-            pass
+
+    def weighted_voter(self, weight: List[float] = None,):
+        pass
+
+    def hard_voter(self):
+        max of list
+        pass
+
+    def 
